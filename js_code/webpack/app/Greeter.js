49afedc553ff91974117d928696581f1e9611cfb
mukeke <muke123com@163.com>
2020-02-01 20:29:22 +0800

    module

--- js_code/webpack/app/Greeter.js
+++ js_code/webpack/app/Greeter.js
@@ -1,5 +1,20 @@
-module.exports = () => {
-    let greet = document.createElement("div");
-    greet.innerText = "Hi there and greetings!";
-    return greet;
+import React, { Component } from 'react';
+import config from './config.json';
+
+class Greeter extends Component {
+    render() { 
+        return ( 
+        <h1>{config.greetText}</h1>
+         );
+    }
 }
+ 
+export default Greeter;
+
+
+// const config = require('./config.json')
+// module.exports = () => {
+//     let greet = document.createElement("div");
+//     greet.innerText = config.greetText;
+//     return greet;
+// }
--- js_code/webpack/webpack.config.js
+++ js_code/webpack/webpack.config.js
@@ -1,7 +1,35 @@
 module.exports = {
+    /*********基本配置***********/
     entry: __dirname + '/app/main.js',
     output: {
         path: __dirname + '/public',
         filename: 'bundle.js'
+    },
+    /*********sourcemaps***********/
+    devtool: 'source-map',
+    /*********本地服务器***********/
+    devServer: {
+        contentBase: './public',
+        historyApiFallback: true,
+        inline: true
+    },
+    /*********loaders***********/
+    module: {
+        rules: [{
+            test: /(\.jsx|\.js)$/,
+            use: {
+                loader: "babel-loader",
+            },
+            exclude: /node_modules/
+        },{
+            test: /\.css$/,
+            use: [
+                {
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }
+            ]
+        }]
     }
 }
